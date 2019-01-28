873b7e32fcefc7e2d1de7c9a6af9a6022c75d2e8
Preston Carey <pcarey1213@gmail.com>
2019-01-28 18:01:49 -0500

    liri commit

---
+++ liri.js
@@ -0,0 +1,162 @@
+require("dotenv").config();
+
+var keys = require("./keys.js");
+
+var fs = require("fs")
+
+var Spotify = require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+var axios = require("axios");
+
+var input = []
+
+for (var i=2; i <process.argv.length; i++){
+
+    input.push(process.argv[i]);
+}
+
+console.log(input);
+
+var command = input[0];
+
+console.log(command);
+
+var item = ""
+
+for (var j = 1; j < input.length; j++) {
+
+    if (j > 1 && j < input.length) {
+      item = item + "+" + input[j];
+    }
+    else {
+      item += input[j];
+  
+    }
+  };
+
+console.log(item);
+
+switch (command) {
+case "concert-this":
+    concertSearch();
+    break;
+
+case "spotify-this-song":
+    spotifySong();
+    break;
+
+case "movie-this":
+    movieSearch();
+    break;
+
+case "do-what-it-says":
+    textCommand();
+    break;
+}
+
+function concertSearch() {
+
+    var artist=item;
+
+    console.log(artist);
+
+    var queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+
+    axios.get(queryUrl).then(    
+        function(response) {
+        console.log(response.data);
+    })
+    .catch(function(err) {
+        console.log(err);
+    })
+};
+
+function spotifySong() {
+
+    var song=item.trim().replace("+", " ")
+
+    console.log(song)
+
+    spotify.search({ type: 'track', query: song, limit:1 }, function(err, data) {
+            
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        };
+
+        console.log(data.tracks.items); 
+
+    })
+};
+
+function movieSearch(){
+
+    var movieName=item;
+
+    if(item===""){
+        movieName="mr+nobody"
+    }
+
+    var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+
+    axios.get(queryUrl).then(
+    function(response) {
+        console.log(response);
+    //   * Title of the movie.
+    //   * Year the movie came out.
+    console.log("The movie was released in: " + response.data.Year);
+    //   * IMDB Rating of the movie.
+    console.log("The movie's rating is: " + response.data.imdbRating);
+    //   * Rotten Tomatoes Rating of the movie.
+    //   * Country where the movie was produced.
+    //   * Language of the movie.
+    //   * Plot of the movie.
+    //   * Actors in the movie.
+    }
+    ).catch(function(err) {
+        console.log(err);
+    });
+    };
+
+function textCommand() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        }
+      
+        input = data.trim().split(",");
+
+        command=input[0];
+
+        for (var j = 1; j < input.length; j++) {
+
+            if (j > 1 && j < input.length) {
+              item = item + "+" + input[j];
+            }
+            else {
+              item += input[j];
+            }
+          };
+
+        switch (command) {
+            case "concert-this":
+                concertSearch();
+                break;
+            
+            case "spotify-this-song":
+                spotifySong();
+                break;
+            
+            case "movie-this":
+                movieSearch();
+                break;
+            
+            case "do-what-it-says":
+                textCommand();
+                break;
+            }
+      
+      });
+};
